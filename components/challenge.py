a416e933ef23ad22fb12a88683d84d104a29954f
sariyyan <sariyyan.ikp@gmail.com>
2025-06-12 00:14:57 +0700

    Update challenge.py

--- components/challenge.py
+++ components/challenge.py
@@ -1,1 +1,22 @@
 # OOP Class untuk fitur challenge detox
+class ChallengeDetox:
+    def __init__(self, challenge_name: str, duration: int):
+        self.challenge_name = challenge_name
+        self.duration = duration
+        self.start_time = None
+        self.end_time = None
+        self.status = "not started"
+
+    def start_challenge(self):
+        self.start_time = datetime.now()
+        self.end_time = self.start_time + timedelta(minutes=self.duration)
+        self.status = "started"
+
+    def end_challenge(self):
+        self.end_time = datetime.now()
+        self.status = "success"
+        return int((self.end_time - self.start_time).total_seconds() / 60)
+
+    def cancel_challenge(self):
+        self.status = "cancelled"
+        self.end_time = datetime.now()
